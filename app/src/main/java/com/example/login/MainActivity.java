ceff061447f1cfc32220b03abc96076999943ff1
hp <chhabraashish123@gmail.com>
2019-04-03 18:46:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.login;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText e1;
+    EditText e2;
+    Button b1;
+    TextView t1;
+    ProgressDialog progressDialog;
+    FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firebaseAuth=FirebaseAuth.getInstance();
+
+      /*  if(firebaseAuth.getCurrentUser()!=null)
+        {
+            finish(); // to end the current activity
+            Intent i= new Intent(MainActivity.this,activity1.class);
+            startActivity(i);
+            //user is already logged in
+        }*/
+        e1=(EditText) findViewById(R.id.editText);
+
+        e2=(EditText) findViewById(R.id.editText2);
+        b1=(Button) findViewById(R.id.b1);
+        t1=(TextView)findViewById(R.id.signin);
+        /*b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             registeruser();
+            }
+        });
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in=new Intent(MainActivity.this,signup.class);
+                startActivity(in);
+
+            }
+        });*/
+        progressDialog=new ProgressDialog(this);
+    }
+    void registered(View v){
+        Intent in=new Intent(MainActivity.this,signup.class);
+        startActivity(in);
+
+    }
+    void registeruser(View v) {
+        String email = e1.getText().toString().trim();
+        String password = e2.getText().toString().trim();
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "PLEASE ENTER EMAIL", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "PLEASE ENTER PASSWORD", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressDialog.setMessage("REGISTERING USER...");
+        progressDialog.show();
+          firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "REGISTERED SUCCESSFULLY", Toast.LENGTH_SHORT).show();
+                            finish(); // to end the current activity
+                            Intent i = new Intent(MainActivity.this, activity1.class);
+                            startActivity(i);
+                        } else {
+                            Toast.makeText(MainActivity.this, "COULD NOT REGISTER, TRY AGAIN!", Toast.LENGTH_SHORT).show();
+                        }
+                        progressDialog.dismiss();
+                    }
+                }
+        );
+    }
+
+
+}
---
+++ app/src/main/java/com/example/login/signup.java
@@ -0,0 +1,84 @@
+package com.example.login;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class signup extends AppCompatActivity  {
+
+    EditText e3;
+    EditText e4;
+    Button b2;
+    TextView t2;
+    ProgressDialog progressDialog;
+    FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+       firebaseAuth=FirebaseAuth.getInstance();
+       /* if(firebaseAuth.getCurrentUser()!=null)
+        {
+            finish(); // to end the current activity
+            Intent i= new Intent(signup.this,activity1.class);
+            startActivity(i);
+            //user is already logged in
+        }*/
+        e3=(EditText)findViewById(R.id.e1);
+        e4=(EditText)findViewById(R.id.e2);
+        b2=(Button)findViewById(R.id.b2);
+        t2=(TextView)findViewById(R.id.signup);
+
+
+      progressDialog=new ProgressDialog(this);
+    }
+    void login(View v)
+    {
+        Intent i= new Intent(signup.this,MainActivity.class);
+        startActivity(i);
+    }
+   void userlogin(View v)
+   {
+       String email=e3.getText().toString().trim();
+       String password=e4.getText().toString().trim();
+       if(TextUtils.isEmpty(email))
+       {
+           Toast.makeText(this, "PLEASE ENTER EMAIL", Toast.LENGTH_SHORT).show();
+           return;
+       }
+       if(TextUtils.isEmpty(password))
+       {
+           Toast.makeText(this, "PLEASE ENTER PASSWORD", Toast.LENGTH_SHORT).show();
+           return;
+       }
+       progressDialog.setMessage("REGISTERING USER...");
+       progressDialog.show();
+       firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                   @Override
+                   public void onComplete(@NonNull Task<AuthResult> task) {
+                      if(task.isSuccessful())
+                      {
+                          finish(); // to end the current activity
+                          Intent i= new Intent(signup.this,activity1.class);
+                          startActivity(i);
+                      }
+
+                   }
+               });
+       }
+
+}
